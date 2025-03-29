bf4171fc7d336920eae6132671ef5282767ba16d
blingblingdong <whoareyouman0.4@gmail.com>
2025-03-30 00:13:40 +0800

    kkk

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    allowedHosts: ['nodessr-production.up.railway.app']
+  }
 })
+
